b9424e55a4a846f4043ad5406797a23805fc3c17
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-12-03 23:53:19 -0500

    Initial Commit

---
+++ bma-week-12-ObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  bma-week-12-ObjectiveC
+//
+//  Created by Wally on 12/3/12.
+//  Copyright (c) 2012 Mooncai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
